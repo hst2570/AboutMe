255d8fd9c3b3e79b03c939c172465d0ba030fb8e
hst2570 <hst2570@gmail.com>
2016-08-26 19:50:21 +0900

    add project + project.imgs

--- project/projectController.js
+++ project/projectController.js
@@ -1,3 +1,15 @@
-myApp.controller('projectController',  ($scope) =>{
-    $scope.desc = "what?";
-})
+myApp.controller('projectController', ['$scope','$http', ($scope, $http) => {
+  $scope.datas = {};
+
+  $http({
+    method : 'GET',
+    url : 'project/projectData.json'
+  }).success((data, status, headers, config)=>{
+    $scope.datas = data.project;
+    console.log("project data list : ")
+    console.log($scope.datas);
+  }).error((data, status, headers, config)=>{
+    console.error(status);
+  });
+
+}]);
